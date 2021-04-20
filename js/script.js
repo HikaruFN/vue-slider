2f676e6b5bce6cab0c8dd653be57b5999da05fc4
HikaruFN <francesconisi95@gmail.com>
2021-04-20 15:27:18 +0200

    fine carousel chevron

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-new app = new Vue(
+var app = new Vue(
     {
         el: '#root',
-    },
+        data: {
+                //Stabilisco immagine default per il carousel
+                defaultImg: 0,
+                //creo array contenete le immagini del carousel
+                carouselImg: ['img/campagna.jpg','img/cascate.webp','img/fiori.jpeg','img/montagne.webp'],               
+        },
+        methods: {
+                chevronRight: function(){
+                    if(this.defaultImg > this.carouselImg.length - 2){
+                        this.defaultImg = 0;
+                    }else{
+                        this.defaultImg = this.defaultImg + 1;
+                    }
+                },
+                chevronLeft: function(){
+                    if(this.defaultImg == 0){
+                        this.defaultImg = this.carouselImg.length -1;
+                    }else{
+                        this.defaultImg = this.defaultImg -1;
+                    }
+                }
+        }
+    }
 );
+
